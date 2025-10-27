28408369fb8a7b889a5a0ba5039ff988825b601a
smorlet <simonmorlet0@gmail.com>
2025-10-27 16:30:19 +0100

    xqcq

---
+++ iut-lyon2-grade-calculator/selenium_part/driver_setup.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://iut-extranet.univ-lyon2.fr")
